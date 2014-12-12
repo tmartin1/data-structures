5f8b68f4f6041a2c7191b715ca613636485cdb5a
tmartin1 <timothy.m.martin@gmail.com>
2014-12-11 16:29:57 -0800

    Completed LinkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    if (list.head !== null) {
+      var headVal = list.head.value;
+      list.head = list.head.next;
+      return headVal;
+    }
   };
 
   list.contains = function(target){
-  };
+    var pointer = list.head;
 
+    if (list.head === null) {
+      return false;
+    }
+    if (pointer.value === target) {
+      return true;
+    }
+    while (pointer.next !== null) {
+      if (pointer.next.value === target) {
+        return true;
+      }
+      pointer = pointer.next;
+    }
+    return false;
+  };
   return list;
 };
 
